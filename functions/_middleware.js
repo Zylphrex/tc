011ddd6d726824bcd818dad6eebc9ab68b8676a8
Tony Xiao <zylphrex@gmail.com>
2026-04-13 15:35:04 -0400

    date input

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -3,14 +3,72 @@ export async function onRequest(context) {
   const { pathname } = new URL(request.url);
   const cookie = request.headers.get("cookie") || "";
 
-  if (request.method == "POST" && pathname === "/cfp_login") {
+  if (request.method == "POST" && pathname === "/login") {
     return next();
   }
 
-  return new Response(`this should ask for password:\ncookies: ${cookie}`, {
+  return new Response(loginPage(), {
     headers: {
       "content-type": "text/html",
       "cache-control": "no-cache",
     },
   });
+}
+
+function loginPage() {
+  return `
+  <!doctype html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>Christina & Tony</title>
+      <meta name="description" content="Enter the event date to access the details">
+    </head>
+
+    <body>
+      <main>
+        <article>
+          <hgroup>
+            <h1>Date</h1>
+            <h2>Please the event date to access the details.</h2>
+          </hgroup>
+          <form method="post" action="/login">
+            ${loginForm()}
+            <button type="submit">Enter</button>
+          </form>
+        </article>
+      </main>
+    </body>
+
+  </html>
+`;
+}
+
+function loginForm() {
+  const months = Array.from({ length: 12 }, (_, i) => i + 1);
+  const days = Array.from({ length: 31 }, (_, i) => i + 1);
+
+  return `
+    <form method="POST" action="/login">
+      <label for="month">Month</label>
+      <select name="month" id="month">
+        ${months
+          .map((month) => {
+            const value = month.toString().padStart(2, "0");
+            return `<option value="${value}">${value}</option>`;
+          })
+          .join("")}
+      </select>
+      <label for="day">day</label>
+      <select name="day" id="day">
+        ${days
+          .map((day) => {
+            const value = day.toString().padStart(2, "0");
+            return `<option value="${value}">${value}</option>`;
+          })
+          .join("")}
+      </select>
+    </form>
+  `;
 }
