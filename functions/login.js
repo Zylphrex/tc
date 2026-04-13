1563d06250eff35b1c52c490133030e1e322145b
Tony Xiao <zylphrex@gmail.com>
2026-04-13 16:54:21 -0400

    typo

--- functions/login.js
+++ functions/login.js
@@ -1,17 +1,18 @@
 import { getEventDateCookie } from "./util.js";
+import { COOKIE_EVENT_DATE_MAX_AGE } from "./constants.js";
 
 export async function onRequestPost(context) {
   const { env, request } = context;
   const body = await request.formData();
   const { day, month } = Object.fromEntries(body);
   const eventDate = `${month}-${day}`;
-  const cookieKeyValue = getEventDateCookie(eventDate);
 
   if (eventDate === env.EVENT_DATE) {
+    const cookieKeyValue = getEventDateCookie(eventDate);
     return new Response("", {
       status: 302,
       headers: {
-        "Set-Cookie": `${cookieKeyValue}; Max-Age=${COOKIE_MAX_AGE}; Path=/; HttpOnly; Secure`,
+        "Set-Cookie": `${cookieKeyValue}; Max-Age=${COOKIE_EVENT_DATE_MAX_AGE}; Path=/; HttpOnly; Secure`,
         "Cache-Control": "no-cache",
         Location: "/",
       },
